05108993e6c0d4c651b91fbca1e3c25870782d79
zameer <zameer40803@gmailo.com>
2023-09-17 22:11:08 -0400

    added print message to assignment file

--- src/Students.java
+++ src/Students.java
@@ -1,6 +1,17 @@
-public class Main {
+public class Students {
     public static void main(String[] args) {
+        
+            double balance = 5000.0;
+            double INTEREST_RATE = .17;
+            double firstMonth = 0;
+            double secondMonth = 0;
+            firstMonth = balance + (balance * INTEREST_RATE);
+            secondMonth = firstMonth + (firstMonth * INTEREST_RATE);
+            System.out.println("After first month: " + firstMonth);
+            System.out.println("After second month: " + secondMonth);
+        System.out.println("Hello world");
 
-        System.out.println("Hello world!");
+
+        }
     }
-}
+
